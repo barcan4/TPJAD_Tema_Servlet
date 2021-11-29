e79b69ebbd657b9e540f645da814b0ed7d6f57aa
Barcoaie <florinel.barcan@gmail.com>
2021-11-29 11:55:45 +0200

    created the 4 AS

---
+++ wildfly_server/src/main/java/controller/InstrumentController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import domain.Instrument;
+import repo.Repo;
+
+import java.util.List;
+
+public class InstrumentController {
+    private Repo repo;
+
+    public InstrumentController(Repo repo) {
+        this.repo = repo;
+        init();
+    }
+
+    private void init() {
+        Instrument ins1 = new Instrument("Fender", "guitar", 300.50);
+        Instrument ins2 = new Instrument("Stratovarius", "violin", 1300.50);
+        Instrument ins3 = new Instrument("Marshall", "Pedal", 199.90);
+
+        repo.getInstrumentList().add(ins1);
+        repo.getInstrumentList().add(ins2);
+        repo.getInstrumentList().add(ins3);
+    }
+
+    public void add(String name, String type, double price) {
+        this.repo.getInstrumentList().add(new Instrument(name, type, price));
+    }
+
+    public void delete(int index) {
+        this.repo.getInstrumentList().remove(index - 1);
+        Instrument.count.decrementAndGet();
+    }
+
+    public void update(int index, String name, String type, double price) {
+        Instrument ins = this.repo.getInstrumentList().get(index - 1);
+        ins.setName(name);
+        ins.setType(type);
+        ins.setPrice(price);
+    }
+
+    public List<Instrument> getAll() {
+        return this.repo.getInstrumentList();
+    }
+
+    @Override
+    public String toString() {
+        return "InstrumentController{" +
+                "repo=" + repo +
+                '}';
+    }
+}
